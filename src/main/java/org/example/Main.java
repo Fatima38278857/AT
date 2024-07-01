f11df8fcd217fe5160a894392fd2d03cf28afae7
Fatima38278857 <aksagova.fatima982@gmail.com>
2024-07-01 23:52:27 +0300

    ОООО

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+import static org.example.FlightBuilder.createFlights;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
